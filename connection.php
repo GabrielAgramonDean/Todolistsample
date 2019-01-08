a539345a1ec5353396ec68a4fe6b804ae5643033
gabriel dean <gabrielagramondean@gmail.com>
2019-01-08 16:02:13 +0800

    update

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "todo_app_db";
+$host = "db4free.net";
+$username = "username10";
+$password = "killswitch10";
+$dbname = "todolistapp";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 
